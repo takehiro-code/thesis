0e2443d13f1137b9c79c6a1d938ce49292d271a5
tta46 <tta46@ensc-mcl-32.engineering.sfu.ca>
2021-02-03 03:24:04 -0800

    Fixed including md files. Fixed detector only using local weight but not attempt to download with API. Added optimizing script for dtector.

---
+++ optimize_detect.sh
@@ -0,0 +1,62 @@
+
+class_cat=ClassC
+seq_name=PartyScene
+#class_id=0
+rgb_source_path=/local-scratch/share_dataset/labled_hevc_sequences
+yuv_source_path=/local-scratch/chyomin/HEVC_Common_Test_Sequence
+test_source_path=/local-scratch/tta46/thesis/seq_test
+comp_source_path=/local-scratch/tta46/thesis/seq_comp
+
+rm data/tuning_detector_result.txt
+
+declare -a conf_thres=($(seq 0.1 0.05 0.9))
+declare -a iou_thres=($(seq 0.1 0.05 0.9))
+#declare -a img_size=($(seq 200 20 900))
+declare -a img_size=(640)
+for conf in ${conf_thres[@]}
+do
+  for iou in ${iou_thres[@]}
+  do
+    for img_s in ${img_size[@]}
+    do
+      echo "Running for conf_thres=${conf}, iou_thres=${iou}, img_size=${img_s}"
+      cd yolov3
+      rm output/${class_cat}/${seq_name}/labels/*.txt
+      python3 detect.py\
+          --source ${rgb_source_path}/${class_cat}/${seq_name}/\
+          --weights weights/yolov3.pt\
+          --conf ${conf}\
+          --img-size ${img_s}\
+          --iou-thres ${iou}\
+          --save-txt\
+          --classes 0 41 58 74 77\
+          --project output/${class_cat}\
+          --name ${seq_name}\
+          --exist-ok >/dev/null 2>/dev/null
+      cd ..
+    
+      rm mAP/input/detection-results/*.txt
+      rm mAP/input/ground-truth/*.txt
+      python3 yolo2map.py\
+        --class_cat $class_cat\
+        --seq_name $seq_name\
+        --source_path $rgb_source_path >/dev/null 2>/dev/null
+    
+      cd mAP
+      python3 main.py -na -np -q\
+        --conf_thres ${conf}\
+        --iou_thres ${iou}\
+        --img_size ${img_s}
+      cd ..
+    done
+  done
+done
+
+
+python3 grid_search.py\
+  --input_path data/tuning_detect_result.txt
+
+
+
+
+
--- test.sh
+++ test.sh
@@ -4,20 +4,22 @@ seq_name=PartyScene
 #class_id=0
 rgb_source_path=/local-scratch/share_dataset/labled_hevc_sequences
 yuv_source_path=/local-scratch/chyomin/HEVC_Common_Test_Sequence
-test_source_path=/local-scratch/tta46/workspace/seq_test
-comp_source_path=/local-scratch/tta46/workspace/seq_comp
+test_source_path=/local-scratch/tta46/thesis/seq_test
+comp_source_path=/local-scratch/tta46/thesis/seq_comp
 
 cd yolov3
 rm output/${class_cat}/${seq_name}/labels/*.txt
-python3 detect.py\
-    --source ${rgb_source_path}/${class_cat}/${seq_name}/\
-    --weights weights/yolov3.pt\
-    --conf 0.25\
-    --img-size 640\
-    --iou-thres 0.45\
-    --save-txt\
-    --classes 0 41 58 74 77\
-    --project output/${class_cat}\
-    --name ${seq_name}\
-    --exist-ok
+#python3 detect.py\
+#    --source ${rgb_source_path}/${class_cat}/${seq_name}/\
+#    --weights weights/yolov3.pt\
+#    --conf 0.25\
+#    --img-size 640\
+#    --iou-thres 0.45\
+#    --save-txt\
+#    --classes 0 41 58 74 77\
+#    --project output/${class_cat}\
+#    --name ${seq_name}\
+#    --exist-ok
+
+
 cd ..
